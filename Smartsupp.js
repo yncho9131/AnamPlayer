e03afb18695b7ba4ed0e23672045d1e43f03a436
yncho9131 <57007886+yncho9131@users.noreply.github.com>
2019-10-26 11:40:40 +0900

    Ver.1

---
+++ Smartsupp.js
@@ -0,0 +1,10 @@
+<!-- Smartsupp Live Chat script -->
+
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = 'd27ccd634031db4dd92bca5f0e3bde04f9486c40';
+window.smartsupp||(function(d) {
+var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+c.type='text/javascript';c.charset='utf-8';c.async=true;
+c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
